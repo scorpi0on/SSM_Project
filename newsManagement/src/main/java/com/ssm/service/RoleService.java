384a7b360662c36eb82821c6902c95b0bf6ab2ad
scorpi0on <55422347+scorpi0on@users.noreply.github.com>
2020-08-13 16:34:07 +0800

    update news

--- newsManagement/src/main/java/com/ssm/service/RoleService.java
+++ newsManagement/src/main/java/com/ssm/service/RoleService.java
@@ -1,9 +1,10 @@
 package com.ssm.service;
-
-import com.ssm.po.Role;
-
 import java.util.List;
-
+import com.ssm.po.Role;
+/*
+ * 角色Service层接口
+ */
 public interface RoleService {
-    public List<Role> findRoleList();
+	public List<Role> findRoleList();
+	public Role findRoleWithUsersByRoleId(Integer roleId);
 }
--- newsManagement/src/main/java/com/ssm/service/impl/UserServiceImpl.java
+++ newsManagement/src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -1,56 +1,50 @@
 package com.ssm.service.impl;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.ssm.dao.UserDao;
 import com.ssm.po.User;
 import com.ssm.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-
-public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserDao userDao;
-    @Override
-    public List<User> findUserList(String keywords, Integer userListRoleId) {
-        List<User> userList=this.userDao.selectUserList(keywords, userListRoleId);
-        return userList;
-    }
-
-    @Override
-    public User findUser(String loginName, String password) {
-        User user=this.userDao.findUser(loginName, password);
-        return user;
-    }
-
-    @Override
-    public User getUserByUserId(Integer userId) {
-        User user=this.userDao.getUserByUserId(userId);
-        return user;
-    }
-
-    @Override
-    public User getUserByLoginName(String loginName) {
-        User user=this.userDao.getUserByLoginName(loginName);
-        return user;
-    }
-
-    @Override
-    public int editUser(User user) {
-        return this.userDao.updateUser(user);
-    }
-
-    @Override
-    public int addUser(User user) {
-        return this.userDao.addUser(user);
-    }
-
-    @Override
-    public int delUser(Integer userId) {
-        return this.userDao.delUser(userId);
-    }
-
-    @Override
-    public int setUserStatus(User user) {
-        return userDao.setUserStatus(user);
-    }
+@Service("userService")
+/*
+ * 用户Service接口实现类
+ */
+public class UserServiceImpl implements UserService{
+	//注入UserDao
+	@Autowired
+	private UserDao userDao;
+	@Override
+	public List<User> findUserList(String keywords, Integer userListRoleId) {
+		List<User> userList=this.userDao.selectUserList(keywords,userListRoleId);
+		return userList;
+	}
+	@Override
+	public User findUser(String loginName, String password) {
+		User user=this.userDao.findUser(loginName, password);
+		return user;
+	}
+	@Override
+	public User getUserByUserId(Integer userId) {
+		return this.userDao.getUserByUserId(userId);
+	}
+	@Override
+	public User getUserByLoginName(String loginName) {
+		return this.userDao.getUserByLoginName(loginName);
+	}
+	@Override
+	public int addUser(User user) {
+		return userDao.addUser(user);
+	}
+	@Override
+	public int editUser(User user) {
+		return this.userDao.updateUser(user);
+	}
+	@Override
+	public int delUser(Integer userId) {
+		return userDao.delUser(userId);
+	}
+	@Override
+	public int setUserStatus(User user) {
+		return userDao.setUserStatus(user);
+	}
 }
